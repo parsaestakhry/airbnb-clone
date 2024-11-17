ebbf527a9254039e21a8986303b527eb6b9dfc61
Afruzoa <afruzoa1999@gmail.com>
2024-11-18 00:13:42 +0330

    fix sign in drop down

---
+++ airbnb-clone/src/main.ts
@@ -0,0 +1,5 @@
+const hamburger = document.querySelector(".hamburger") as HTMLElement;
+const menu = document.querySelector(".menu") as HTMLElement;
+hamburger.addEventListener("click", () => {
+menu.classList.toggle("active");
+});
