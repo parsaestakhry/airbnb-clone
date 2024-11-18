7122ed39729c441046da361ad4683800edefd2ec
Afruzoa <afruzoa1999@gmail.com>
2024-11-18 16:00:47 +0330

    fix js

--- airbnb-clone/src/main.ts
+++ airbnb-clone/src/main.ts
@@ -6,22 +6,3 @@ menu.classList.toggle("active");
 const slider = document.querySelector('.icon-item');
 const nextBtn = document.querySelector('.next-btn');
 const prevBtn = document.querySelector('.prev-btn');
-
-let currentIndex = 0;
-const iconWidth = 110;
-const visibleIcons = Math.floor(window.innerWidth / iconWidth); 
-const totalIcons = document.querySelectorAll('.icon').length;
-
-nextBtn.addEventListener('click', () => {
-    if (currentIndex < totalIcons - visibleIcons) {
-        currentIndex++;
-        slider.style.transform = `translateX(-${currentIndex * iconWidth}px)`;
-    }
-});
-
-prevBtn.addEventListener('click', () => {
-    if (currentIndex > 0) {
-        currentIndex--;
-        slider.style.transform = `translateX(-${currentIndex * iconWidth}px)`;
-    }
-});
