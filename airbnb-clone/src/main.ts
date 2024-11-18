8929c23f9089affc6f454c4857a8d8553f012527
Afruzoa <afruzoa1999@gmail.com>
2024-11-18 16:39:04 +0330

    fix UI

--- airbnb-clone/src/main.ts
+++ airbnb-clone/src/main.ts
@@ -3,6 +3,3 @@ const menu = document.querySelector(".menu") as HTMLElement;
 hamburger.addEventListener("click", () => {
 menu.classList.toggle("active");
 });
-const slider = document.querySelector('.icon-item');
-const nextBtn = document.querySelector('.next-btn');
-const prevBtn = document.querySelector('.prev-btn');
