c12d958b416707d1fa0adc471f372c34f3d3b8c8
Afruzoa <afruzoa1999@gmail.com>
2024-11-30 03:07:19 +0330

    show tab

--- airbnb-clone/src/main.ts
+++ airbnb-clone/src/main.ts
@@ -57,3 +57,21 @@ const activeSearchItems = (selectedItem: Element) => {
   };
 
   searchItems.forEach(item=> activeSearchItems(item))
+
+  // 
+  const tabs = document.querySelectorAll('[data-tab-target]')
+  const tabContents = document.querySelectorAll('[data-tab-content]')
+  
+  tabs.forEach(tab=> {
+    tab.addEventListener('click', () => {
+      const target = document.querySelector(tab.dataset.tabTarget)
+      tabContents.forEach(tabContent => {
+        tabContent.classList.remove('active')
+      })
+      tabs.forEach(tab => {
+        tab.classList.remove('active')
+      })
+      tab.classList.add('active')
+      target.classList.add('active')
+    })
+  })
